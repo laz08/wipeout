1a86fed384f3dccf12f17beb516c51f16cba3965
AlecLafita <aleclafitarf@gmail.com>
2016-11-12 15:05:11 +0100

    Added some assets and basic movement script

---
+++ Assets/Scripts/MoveVehicle.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveVehicle : MonoBehaviour {
+
+	public float accZ = 6.0f;
+	public float maxSpeedForward = 100.0f;
+
+	float speedZ = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			speedZ += accZ * Time.deltaTime;
+			speedZ = Mathf.Min (speedZ,maxSpeedForward);
+		} else {
+			speedZ -= accZ * Time.deltaTime;
+			speedZ = Mathf.Max (speedZ,0.0f);
+		}
+
+		if (Input.GetKey (KeyCode.DownArrow)) {
+			speedZ = -accZ;
+		}
+			
+		gameObject.transform.Translate(0.0f, 0.0f, speedZ*Time.deltaTime + (1/2)*accZ*Time.deltaTime*Time.deltaTime);
+
+	}
+		
+}
+
---
+++ Assets/VehiclesModels/Corvette/Readme.txt
@@ -0,0 +1,15 @@
+Sci-Fi Corvette Model 
+
+Corvette - 10k vertices 
+
+All textures 2048x2048 (Albedo, Specular and Normal) 
+
+_______ 
+
+LINKS: 
+
+Sketchfab 
+
+I'll be glad if you like the model and i can continue to create a new model,
+
+Thanks
