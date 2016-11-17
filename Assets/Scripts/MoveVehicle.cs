942ff081ae2c835f573a7c01584706260a8b7574
AlecLs <alec.lafitarf@gmail.com>
2016-11-17 17:42:31 +0100

    Froozen the rotation by the forces. WIP, change rotation when normal of the actual terrain changes

--- Assets/Scripts/MoveVehicle.cs
+++ Assets/Scripts/MoveVehicle.cs
@@ -6,17 +6,13 @@ public class MoveVehicle : MonoBehaviour {
 	public float accZ = 6.0f;
 	public float maxSpeedForward = 100.0f;
 	public float turnSpeed = 1.0f;
-	public float hoovingHeight = 2.0f;
-	public float hoovingForce = 10.0f;
+    public float fallFactor = 0.5f;
 
 	private bool previousStateGoForward = true;
 	private float speedZ = 0.0f;
 	private float actRotation = 0.0f;
-	private Rigidbody vehicleRigidBody;
 
-	void Awake() {
-		vehicleRigidBody = GetComponent<Rigidbody> ();
-	}
+
 
 	// Update is called once per frame
 	void Update () {
@@ -44,49 +40,50 @@ public class MoveVehicle : MonoBehaviour {
 			}
 		}
 			
-		//gameObject.transform.Translate(0.0f, 0.0f, speedZ*Time.deltaTime + (1/2)*accZ*Time.deltaTime*Time.deltaTime,Space.Self);
+		gameObject.transform.Translate(0.0f, 0.0f, speedZ*Time.deltaTime + (1/2)*accZ*Time.deltaTime*Time.deltaTime,Space.Self);
 
 		//Tiling
-		/*float turn = Input.GetAxis("Horizontal");
-		gameObject.transform.Rotate(0.0f, -actRotation,0.0f,Space.Self);
-		actRotation += turn*turnSpeed;
-		gameObject.transform.Rotate(0.0f, actRotation,0.0f,Space.Self);*/
+        float turn = Input.GetAxis("Horizontal");
+        gameObject.transform.Rotate(0.0f, -actRotation, 0.0f, Space.Self);
+        actRotation += turn * turnSpeed;
+        gameObject.transform.Rotate(0.0f, actRotation, 0.0f, Space.Self);
+
+
+        changeRotation();
+
 	}
-		
+
+    void changeRotation()
+    {
+        Ray ray = new Ray(transform.position, -transform.up);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit)) {
+            /*Vector3 angleIncr = hit.normal - transform.up;
+            //Vector3.Angle(hit.normal,transform.up)
+            Debug.Log(hit.normal);
+            transform.up = Vector3.Lerp(transform.up,hit.normal,Time.deltaTime*5.0f);
+            */
+
+            /*
+            //http://answers.unity3d.com/questions/351899/rotation-lerp.html
+            Vector3 newRotation = Quaternion.LookRotation(hit.normal, -transform.forward).eulerAngles;
+            newRotation.x = 0;
+            newRotation.z = 0;
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(newRotation), Time.deltaTime);
+           */
+
+            //http://answers.unity3d.com/questions/1192454/bug-transformup-transformup-sets-y-rotation-to-0.html
+            transform.rotation = Quaternion.LookRotation(hit.normal, -transform.forward);
+            transform.Rotate(Vector3.right, 90f);
+
+
+        }
+    }
 
 	void changeAccelerationDirection() {
 		speedZ = -speedZ;
 		accZ *= -1;
-	}
-
-	void FixedUpdate() {
-		//vehicleRigidBody.AddRelativeForce (new Vector3 (0.0f, 0.0f, speedZ), ForceMode.VelocityChange);
-		vehicleRigidBody.AddRelativeForce(new Vector3 (0.0f, 0.0f, speedZ),ForceMode.Force);
-		//vehicleRigidBody.velocity = new Vector3 (0.0f, 0.0f, speedZ);
-		float turn = Input.GetAxis("Horizontal");
-		vehicleRigidBody.AddRelativeTorque (new Vector3(0.0f,turn*turnSpeed,0.0f));
-
-		Vector3 size = GetComponent<BoxCollider> ().bounds.size;
-		//Hooving physics
-		Ray ray = new Ray (transform.position + new Vector3(0, 0, size.z/2) , -transform.up);
-		RaycastHit hit;
-		if (Physics.Raycast(ray, out hit, hoovingHeight)) {
-			float relativeHeight =(hoovingHeight- hit.distance) / hoovingHeight;
-			Vector3 forceToApply =transform.up* relativeHeight * hoovingForce;
-			vehicleRigidBody.AddRelativeForce (forceToApply, ForceMode.Acceleration);
-
-			Vector3 normal = Vector3.Normalize (hit.normal);
-			Debug.Log ("normal" + normal);
-			transform.up = normal;
-			Debug.Log ("up" + transform.up);
-			//transform.up= normal;
-			/*if (hit.distance <=hoovingHeight) {//when is higher than the hooving height, not apply hooving force
-				Vector3 forceToApply = new Vector3(transform.up.x *Physics.gravity.x,
-				transform.up.y *Physics.gravity.y,transform.up.z *Physics.gravity.z)*-2*Time.deltaTime;
-				//vehicleRigidBody.AddForce (forceToApply, ForceMode.VelocityChange);
-				vehicleRigidBody.AddRelativeForce (forceToApply, ForceMode.VelocityChange);
-			}*/
-		}
 	}
 		
 }
