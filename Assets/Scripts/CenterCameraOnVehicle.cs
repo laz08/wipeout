452cd249a7112b7283348e0acfc5fc0fe5205f9e
Laura <lazeru08@gmail.com>
2016-11-18 18:51:59 +0100

    Camera tracking player roughly implemented

---
+++ Assets/Scripts/CenterCameraOnVehicle.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CenterCameraOnVehicle : MonoBehaviour {
+
+	public GameObject mUserVehicle;
+	public float mDefaultZ = -10f;
+	public float mOffsetY = 10f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+
+		transform.position = 
+			new Vector3 (
+			mUserVehicle.transform.position.x, 
+			mUserVehicle.transform.position.y + mOffsetY, 
+			mUserVehicle.transform.position.z + mDefaultZ);
+
+		transform.LookAt(mUserVehicle.transform.position);
+	}
+}
