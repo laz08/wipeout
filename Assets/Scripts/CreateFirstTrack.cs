c622f8d0aa179abcf8503c977f4e2a72d357025d
Laura <lazeru08@gmail.com>
2016-11-18 19:14:36 +0100

    First track improved

--- Assets/Scripts/CreateFirstTrack.cs
+++ Assets/Scripts/CreateFirstTrack.cs
@@ -3,16 +3,29 @@ using System.Collections;
 
 public class CreateFirstTrack : MonoBehaviour {
 	
-	public GameObject mTrackPiece;
+	public GameObject mStraightPiece;
+	public int mStraightNum = 4;
+
+	private float mLastItemSizeZ = 0f;
+	private float mLastItemSizeX = 0f;
 
 	// Use this for initialization
 	void Start () {
-		
-		for (int j = 0; j < 4; j++) {
-			
-		
-			GameObject obj = (GameObject)Instantiate (mTrackPiece, new Vector3 (0f, 0f, j*20f), transform.rotation);
+
+		//Create first straight road
+		GameObject obj;
+		for (int j = 0; j < mStraightNum; j++) {
+
+			//Instantiate object
+			obj = (GameObject) Instantiate (mStraightPiece,
+											new Vector3 (0f, 0f, j*mLastItemSizeZ), transform.rotation);
+			//Place on parent
 			obj.transform.parent = transform;
+
+			//Save values for next item to be instantiated
+			var renderer = obj.GetComponent<Renderer> ();
+			mLastItemSizeZ = renderer.bounds.size.z;
+			mLastItemSizeX = renderer.bounds.size.x;
 		}
 	}
 
