1f58e18dcbcabc4239ee50c8cad9785b4fdf79c6
Laura <lazeru08@gmail.com>
2016-11-19 17:55:42 +0100

    Forcing next level concept

---
+++ Assets/Scripts/ForceNextLevel.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ForceNextLevel : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (Input.GetKey ("n")) {
+			Application.LoadLevel ("TestLevel");
+		}
+	}
+}
