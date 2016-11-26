199539168c4b7b607738516e30a94e7232d9034e
AlecLafita <aleclafitarf@gmail.com>
2016-11-26 18:32:20 +0100

    Added velocity ramps. WIP:Projectile attack item

--- Assets/Scripts/ItemBehaviour.cs
+++ Assets/Scripts/ItemBehaviour.cs
@@ -13,14 +13,12 @@ public class ItemBehaviour : MonoBehaviour {
 			//Start showing the object through transparency
 			if (countdownToRespawn < timeToRespawn / 2.0f) {
 				Color itemColor = gameObject.GetComponent<MeshRenderer>().material.color;
-				Debug.Log (itemColor.a);
-				itemColor.a += 0.007f;
+				itemColor.a += 0.007f;//Should be nice to put this as a function of timeToRespawn
 				gameObject.GetComponent<MeshRenderer>().material.color = itemColor;
 			}
 		}
 		else {
 			countdownToRespawn = 0.0f;
-			gameObject.GetComponent<Renderer>().enabled = true;
 			gameObject.GetComponent<Collider>().enabled = true;
 		}
 		transform.Rotate(0.0f, 50*Time.deltaTime, 0.0f);
@@ -31,7 +29,6 @@ public class ItemBehaviour : MonoBehaviour {
 		Color itemColor = gameObject.GetComponent<MeshRenderer>().material.color;
 		itemColor.a = 0.0f;
 		gameObject.GetComponent<MeshRenderer> ().material.color = itemColor;
-		Debug.Log (gameObject.GetComponent<MeshRenderer> ().material.color.a);
 		gameObject.GetComponent<Collider>().enabled = false;
 	}
 
--- Assets/Scripts/Vehicle/ItemVehicle.cs
+++ Assets/Scripts/Vehicle/ItemVehicle.cs
@@ -4,13 +4,15 @@ using System.Collections;
 public class ItemVehicle : MonoBehaviour {
 
     enum Items {
-           NONE, TURBO
+           NONE, TURBO, PROJECTILE
     };
 
     Items actualItem;
 
-	public float turboTime = 2.0f;
-	private float turboCountDown;
+	public float turboTime = 1.0f;
+	public float turboSpeed = 150.0f;
+	private float turboCountDown = 0.0f;
+	private bool turboActivate = false;
 
 	// Use this for initialization
 	void Start () {
@@ -19,22 +21,93 @@ public class ItemVehicle : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	    if (actualItem!=Items.NONE && (Input.GetKey ("a")) ) {
+
+		//Check if user activates actual item
+	    if (actualItem != Items.NONE && (Input.GetKey ("e")) ) {
+			itemActivatedEffect();
 			if (actualItem == Items.TURBO) {
-				Debug.Log ("trhworing item");
-				//http://answers.unity3d.com/questions/400977/changing-a-variable-in-one-script-using-another-sc.html
-				//GetComponent<MoveVehicle> (MoveVehicle);
+				turboActivate = true;
+				turboCountDown = turboTime;
 				actualItem = Items.NONE;
 			}
+			itemNotActivatedEffect();//Set to NONE
         }
+
+		//Check if the vehicle is in a velocity ramp
+		Ray ray = new Ray(transform.position, -transform.up);
+		RaycastHit hit;
+		if (Physics.Raycast (ray, out hit)) {
+			if (hit.collider.gameObject.tag == "VelocityZone") {
+				turboActivate = true;
+				turboCountDown = turboTime;
+			}
+		}
+
+		//Apply turbo "force"
+		if (turboActivate) {
+			if (turboCountDown <= 0.0f) {
+				turboActivate = false;
+				itemActivatedEffect ();//Set to NONE
+			} else {
+				turboCountDown -= Time.deltaTime;
+				transform.Translate (0.0f,0.0f,turboSpeed*Time.deltaTime,Space.Self);
+				//TODO:add velocity visual effect
+			}
+		}
 	}
 
-    void OnCollisionEnter(Collision collision)
-    {
-        if (actualItem == Items.NONE && collision.gameObject.tag == "PowerUpItem")
-        {
-            actualItem = Items.TURBO;
-			turboCountDown = turboTime;
+    void OnCollisionEnter(Collision collision) {
+        if (actualItem == Items.NONE && collision.gameObject.tag == "PowerUpItem") {
+			int item = Random.Range (0, 2);
+			Debug.Log ("random number: " + item);
+			switch (item) {
+			case 0:
+				actualItem = Items.TURBO;
+				break;
+			case 1:
+				actualItem = Items.PROJECTILE;
+				break;
+			default:
+				break;
+			}
+			itemNotActivatedEffect();
         }
     }
+
+	//GUI effects
+
+	//show actual item (not activated) TODO
+	void itemNotActivatedEffect() {
+		switch (actualItem) {
+		case Items.NONE:
+
+			break;
+		case Items.TURBO:
+
+			break;
+		case Items.PROJECTILE:
+
+			break;
+		default:
+			break;
+		}
+	}
+
+	//show actual item (actived) TODO
+	void itemActivatedEffect() {
+		switch (actualItem) {
+		case Items.NONE:
+
+			break;
+		case Items.TURBO:
+
+			break;
+		case Items.PROJECTILE:
+
+			break;
+		default:
+			break;
+		}
+	}
+
 }
