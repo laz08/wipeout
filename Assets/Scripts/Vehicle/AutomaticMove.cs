959a0254e0911cd73e0532e103a1900373d15678
Laura <lazeru08@gmail.com>
2016-11-29 22:23:46 +0100

    [FEAT] Waypoints created

--- Assets/Scripts/Vehicle/AutomaticMove.cs
+++ Assets/Scripts/Vehicle/AutomaticMove.cs
@@ -2,15 +2,8 @@
 using System.Collections;
 
 public class AutomaticMove : MonoBehaviour {
-
-    public float duration;
-
-    public bool lookForward;
-
-    public SplineWalkerMode mode;
-
-    private float progress;
-    public BezierSpline mSpline;
+	
+	public CreateFirstTrackWaypoints mWaypointsCreator;
 
 	// Use this for initialization
 	void Start () {
@@ -21,33 +14,9 @@ public class AutomaticMove : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-        float velocity = GetComponent<MoveVehicle>().speedZ;
-        progress += Time.deltaTime * 0.001f*velocity;
-        //progress += Time.deltaTime / duration;
-        if (progress > 1f)
-        {
-            if (mode == SplineWalkerMode.Once)
-            {
-                progress = 1f;
-            }
-            else if (mode == SplineWalkerMode.Loop)
-            {
-                progress -= 1f;
-            }
-            else
-            {
-                progress = 2f - progress;
-            }
-        }
-        
-        Debug.Log(progress);
-
-        Vector3 position = mSpline.GetPoint(progress);
-        //transform.localPosition = position;
-        if (lookForward && progress != 0)
-        {
-            transform.LookAt(position + mSpline.GetDirection(progress));
-        }
-	
+		if (Input.GetKey (KeyCode.K)) {
+		
+			transform.position = mWaypointsCreator.getNextWaypoint (transform.position);
+		}
 	}
 }
