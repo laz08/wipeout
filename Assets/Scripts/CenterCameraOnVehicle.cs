5430717b3d6890b41a76b95019402588d5cac766
AlecLafita <aleclafitarf@gmail.com>
2016-12-10 20:02:45 +0100

    Changed camera script

--- Assets/Scripts/CenterCameraOnVehicle.cs
+++ Assets/Scripts/CenterCameraOnVehicle.cs
@@ -4,63 +4,26 @@ using System.Collections;
 public class CenterCameraOnVehicle : MonoBehaviour {
 
 	public GameObject mUserVehicle;
-	public float mDefaultXOffset = -15f;
-	public float mOffsetY = 10f;
-
-    public float roadRadius = 20.0f;
-    public float horizMove = -10.0f;
-
-    private float actualTransl;
+	private float mDefaultXOffset = -4.2f;
+	private float mOffsetY = 15.58f;
+	private float mOffsetZ = -38.7f;
 	// Use this for initialization
 	void Start () {
-        //actualTransl = 0.0f;
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        /*if (Input.GetKey(KeyCode.RightArrow))
-        {
-            actualTransl += horizMove;
-        }
-        else if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            actualTransl -= horizMove;
 
-        }
-        else
-        {
-            if (actualTransl > 0)
-                actualTransl += horizMove ;
-            else if (actualTransl < 0)
-            {
-                actualTransl -= horizMove ;
-            }
-        }
-
-        if (actualTransl > 0)
-        {
-            actualTransl = Mathf.Min(actualTransl, roadRadius);
-        }
-        else if (actualTransl < 0)
-        {
-            actualTransl = Mathf.Max(actualTransl, -roadRadius);
-        }
-
-        transform.Translate(actualTransl * Time.deltaTime, 0.0f, 0.0f);
-        */
-
-
-		/*transform.position = mUserVehicle.transform.position;
+		transform.position = mUserVehicle.transform.position;
 	
-		transform.forward = -mUserVehicle.transform.forward;
-		transform.LookAt(mUserVehicle.transform.position);
-	
+		//transform.forward = -mUserVehicle.transform.forward;
+		//transform.LookAt(mUserVehicle.transform.position);
 		transform.localPosition = 
 			new Vector3 (
-				mUserVehicle.transform.localPosition.x - mDefaultXOffset, 
-				mUserVehicle.transform.localPosition.y + mOffsetY, 
-				mUserVehicle.transform.localPosition.z);
-				*/
+				mDefaultXOffset, 
+				mOffsetY, 
+				mOffsetZ);
+
 	}
 }
