bd09b7ea2fcfe3bdecca0ad9ef4d89d23dc116ba
AlecLafita <aleclafitarf@gmail.com>
2016-12-13 17:59:54 +0100

    Swag added to turbo

---
+++ Assets/Scripts/TurboChange.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurboChange : MonoBehaviour {
+
+	private Renderer rend;
+	private float offset;
+	// Use this for initialization
+	void Start () {
+		rend = GetComponent<Renderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		offset += Time.deltaTime;
+
+		rend.material.SetTextureOffset("_MainTex", new Vector2(0,offset));
+
+	}
+}
