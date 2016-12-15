c5889bda8a74c8f5bd42fd8ede2e576929174641
Laura <lazeru08@gmail.com>
2016-12-15 10:14:29 +0100

    Map selector

---
+++ Assets/Scripts/MenuButtons/MapSelectionController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UnityEngine.SceneManagement;
+
+public class MapSelectionController : MonoBehaviour {
+
+	public GameObject normalTrack;
+	public GameObject torusTrack;
+
+	private static string FIRST_TRACK_NAME = "FirstTrack";
+	private static string SECOND_TRACK_NAME = "TorusTrack";
+
+	private static string VEHICLE_SELECT_SCREEN_NAME = "VehicleSelectScreen";
+
+	public float rotationSpeed = 75;
+
+
+	void Awake() {
+		
+	}
+
+	void Update(){
+
+		normalTrack.transform.Rotate (Vector3.up * Time.deltaTime * rotationSpeed, Space.Self);
+		torusTrack.transform.Rotate (Vector3.up * Time.deltaTime * -rotationSpeed, Space.Self);
+	}
+
+	public void onNormalSelected(){
+
+		SceneManager.LoadScene (FIRST_TRACK_NAME);
+	}
+
+	public void onTorusSelected(){
+
+
+		SceneManager.LoadScene (SECOND_TRACK_NAME);
+	}
+
+	public void onBackSelected(){
+
+		SceneManager.LoadScene(VEHICLE_SELECT_SCREEN_NAME);
+	}
+}
--- Assets/Scripts/MenuButtons/VehicleSelectController.cs
+++ Assets/Scripts/MenuButtons/VehicleSelectController.cs
@@ -8,25 +8,17 @@ public class VehicleSelectController : MonoBehaviour {
 	public GameObject corvetteObj;
 	public GameObject boatObj;
 
-	private static string FIRST_TRACK_NAME = "FirstTrack";
-	private static string SECOND_TRACK_NAME = "TorusTrack";
-	private string selected_Track;
 	private static string GAME_MENU_NAME = "GameMenu";
+	private static string MAP_SCREEN_NAME = "MapSelector";
 
 	private static string SELECTED_VEHICLE_KEY = "selectedVehicle";
+
 
 	private static string KIRBY_VEHICLE = "Kirby";
 	private static string CORVETTE_VEHICLE = "Corvette";
 	private static string BOAT_VEHICLE = "Boat";
 
 	public float rotationSpeed = 75;
-
-
-	void Awake() {
-	 	//get here map parameter 
-		//string paramTrack = AssemblyCSharp.SceneController.getParam (SELECTED_TRACK_KEY);
-		selected_Track = FIRST_TRACK_NAME;
-	 }
 
 	void Update(){
 
@@ -37,17 +29,17 @@ public class VehicleSelectController : MonoBehaviour {
 
 	public void onKirbySelected(){
 
-		AssemblyCSharp.SceneController.Load (selected_Track, SELECTED_VEHICLE_KEY, KIRBY_VEHICLE);
+		AssemblyCSharp.SceneController.Load (MAP_SCREEN_NAME, SELECTED_VEHICLE_KEY, KIRBY_VEHICLE);
 	}
 
 	public void onCorvetteSelected(){
 	
-		AssemblyCSharp.SceneController.Load (selected_Track, SELECTED_VEHICLE_KEY, CORVETTE_VEHICLE);
+		AssemblyCSharp.SceneController.Load (MAP_SCREEN_NAME, SELECTED_VEHICLE_KEY, CORVETTE_VEHICLE);
 	}
 
 	public void onBoatSelected(){
 	
-		AssemblyCSharp.SceneController.Load (selected_Track, SELECTED_VEHICLE_KEY, BOAT_VEHICLE);
+		AssemblyCSharp.SceneController.Load (MAP_SCREEN_NAME, SELECTED_VEHICLE_KEY, BOAT_VEHICLE);
 	}
 
 	public void onBackSelected(){
